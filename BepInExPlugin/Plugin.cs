6c8f444dc65408eeb524573958d5d192c71605b0
JW Modder <jade.wayfarer@gmail.com>
2026-06-19 23:21:37 +0300

    v 1.0.5

--- BepInExPlugin/Plugin.cs
+++ BepInExPlugin/Plugin.cs
@@ -132,7 +132,7 @@ public class Plugin : BasePlugin
         IL2CPPBase.Initialize(this);
     }
 
-    //configNoPlayerDamage
+    // configNoPlayerDamage
 
     [HarmonyPatch(typeof(HealthComponent_Base), nameof(HealthComponent_Base.DealDamage))]
     class HealthComponentPatch4
@@ -140,33 +140,34 @@ public class Plugin : BasePlugin
         static bool Prefix(ref float damageToDeal, HealthComponent_Base __instance)
         {
             if (!configNoPlayerDamage.Value || __instance.m_ParentPlayer == null)
-            {
-                //Log.LogInfo("PLAYER DAMAGE ALLOWED 4 - Damage: " + damageToDeal.ToString("0.00"));
                 return true;
-            }
-            // Skip original
             damageToDeal = 0;
-            //Log.LogInfo("!!NO PLAYER DAMAGE ALLOWED 4!!");
             return true;
         }
     }
 
-    //configNoPlayerShipHealthDamage
+    // configNoPlayerShipHealthDamage
 
-    // Playership_DamageController ApplyDamageToShipCore(int damage)
     [HarmonyPatch(typeof(Playership_DamageController), nameof(Playership_DamageController.ApplyDamageToShipCore))]
-    [HarmonyPatch(typeof(Playership_DamageController), nameof(Playership_DamageController.DelayedDamage))]
     class Playership_DamageControllerPatch2
     {
         static bool Prefix(ref int damage, ref Playership_DamageController __instance)
         {
             if (!configNoPlayerShipHealthDamage.Value)
-            {
-                //Log.LogInfo("SHIP CORE DAMAGE ALLOWED - Damage value: " + damage);
                 return true;
-            }
             damage = 0;
-            //Log.LogInfo("!!NO SHIP CORE DAMAGE ALLOWED!! - Damage value: " + damage);
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Playership_DamageController), nameof(Playership_DamageController.DelayedDamage))]
+    class Playership_DamageControllerPatch3
+    {
+        static bool Prefix(ref int damage, ref Playership_DamageController __instance)
+        {
+            if (!configNoPlayerShipHealthDamage.Value)
+                return true;
+            damage = 0;
             return true;
         }
     }
@@ -177,16 +178,12 @@ public class Plugin : BasePlugin
         static void Postfix(ref shipref __instance)
         {
             if (!configNoPlayerShipHealthDamage.Value)
-            {
-                //Log("SHIP CORE DELAYED DAMAGE ALLOWED - Damage value: " + damage);
                 return;
-            }
             if (__instance.m_SpaceShip_StateBlackboardData != null)
                 if (__instance.m_SpaceShip_StateBlackboardData.m_ShipHealth.Value < __instance.m_SpaceShip_StateBlackboardData.m_ShipMaxHealth.Value)
                 {
                     int health = __instance.m_SpaceShip_StateBlackboardData.m_ShipHealth.Value;
                     int maxHealth = __instance.m_SpaceShip_StateBlackboardData.m_ShipMaxHealth.Value;
-                    //Log("!!NO SHIP HEALTH DAMAGE ALLOWED!! - Health value: " + health + " max health: " + maxHealth);
                     __instance.m_SpaceShip_StateBlackboardData.m_ShipHealth.Value = __instance.m_SpaceShip_StateBlackboardData.m_ShipMaxHealth.Value;
                 }
 
@@ -199,17 +196,13 @@ public class Plugin : BasePlugin
         static bool Prefix(ref SpaceShip_BaseComponent.ComponentHealthStatus newCritical)
         {
             if (!configNoPlayerShipHealthDamage.Value)
-            {
-                //Log.LogInfo("SHIP DAMAGE ALLOWED - New component status: " + newCritical.ToString());
                 return true;
-            }
             newCritical = SpaceShip_BaseComponent.ComponentHealthStatus.Healthy;
-            //Log.LogInfo("!!NO SHIP DAMAGE ALLOWED!! - New component status: " + newCritical.ToString());
             return true;
         }
     }
 
-    //configPlayerShipDamageMultiplier
+    // configPlayerShipDamageMultiplier
 
     [HarmonyPatch(typeof(SpaceShip_Cannon_Base), nameof(SpaceShip_Cannon_Base.SafeStart))]
     class SpaceShip_Cannon_BasePatch1
@@ -225,7 +218,7 @@ public class Plugin : BasePlugin
         static Dictionary<int, ShipCannon> shipCannonsDict = new();
         static bool Prefix(ref SpaceShip_Cannon_Base __instance)
         {
-            if (configPlayerShipDamageMultiplier.Value <= 1 || __instance.TracerProjectileData == null)
+            if (configPlayerShipDamageMultiplier.Value <= 1f || __instance.TracerProjectileData == null)
             {
                 Log.LogInfo("SHIP SafeStart CANNON DAMAGE NORMAL");
                 return true;
@@ -258,9 +251,7 @@ public class Plugin : BasePlugin
         }
     }
 
-    //configNoPlayerShipShieldDamage
-
-    // Keepsake.ShieldUnitsManager !!! DealDamageToShields(int finalDamage, DamageInfo damageInfo, out int coreDamage)
+    // configNoPlayerShipShieldDamage
 
     [HarmonyPatch(typeof(ShieldUnitController), nameof(ShieldUnitController.OnPlateHealthChanged))]
     class ShieldUnitControllerPatch1
@@ -268,21 +259,17 @@ public class Plugin : BasePlugin
         static void Postfix(ref int plateHealth, ref ShieldUnitController __instance)
         {
             if (!configNoPlayerShipShieldDamage.Value)
-            {
-                //Log.LogInfo("SHIP SHIELD DAMAGE ALLOWED");
                 return;
-            }
             if (plateHealth < __instance.MaxHealth)
             {
                 plateHealth = __instance.MaxHealth;
                 __instance.m_ShieldUnitBlackboardData.m_IsShieldUnitActivated.Value = true;
                 __instance.m_ShieldUnitBlackboardData.m_ShieldUnitHealth.Value = __instance.MaxHealth;
-                //Log.LogInfo("SHIP SHIELD STRENGTH: " + plateHealth);
             }
         }
     }
 
-    //configNoShipAmmoCost
+    // configNoShipAmmoCost
 
 
     [HarmonyPatch(typeof(PlayerShip_IndividualTurretController), nameof(PlayerShip_IndividualTurretController.ExpendAmmunition))]
@@ -292,17 +279,13 @@ public class Plugin : BasePlugin
         static bool Prefix(ref float ammoUsagePerRound)
         {
             if (!configNoShipAmmoCost.Value)
-            {
-                //Log.LogInfo("SHIP AMMO COST NORMAL - Cost value: " + ammoUsagePerRound.ToString());
                 return true;
-            }
             ammoUsagePerRound = 0;
-            //Log.LogInfo("SHIP AMMO COST ZERO - Cost value: " + ammoUsagePerRound.ToString());
             return true;
         }
     }
 
-    //configNoCraftCost
+    // configNoCraftCost
 
     [HarmonyPatch(typeof(Assembler), nameof(Assembler.CraftBlueprint))]
     class Assembler_CraftBlueprintPatch1
@@ -310,17 +293,13 @@ public class Plugin : BasePlugin
         static bool Prefix(ref UnlockableBlueprint blueprint)
         {
             if (!configNoCraftCost.Value)
-            {
-                //Log.LogInfo("CRAFT COST NORMAL - Cost value: " + blueprint.m_MateriaCraftCost.ToString());
                 return true;
-            }
             blueprint.m_MateriaCraftCost = 0;
-            //Log.LogInfo("CRAFT COST ZERO - Cost value: " + blueprint.m_MateriaCraftCost.ToString());
             return true;
         }
     }
 
-    //configMateriaMultiplier
+    // configMateriaMultiplier
 
     [HarmonyPatch(typeof(Disassembler), nameof(Disassembler.Disassemble))]
     class DisassemblePatch1
@@ -329,36 +308,25 @@ public class Plugin : BasePlugin
         static bool Prefix(ref int materiaValue)
         {
             if (configMateriaMultiplier.Value <= 1)
-            {
-                //Log.LogInfo("MATERIA MULTIPLIER NORMAL - Materia disassemble value: " + materiaValue.ToString());
                 return true;
-            }
-            materiaValue = (int)(materiaValue * configMateriaMultiplier.Value);
-            //Log.LogInfo("MATERIA MULTIPLIER " + configMateriaMultiplier.Value.ToString() + " - Materia disassemble value: " + materiaValue.ToString());
+            materiaValue = Mathf.RoundToInt(materiaValue * configMateriaMultiplier.Value);
             return true;
         }
     }
 
-    //configNoPlayerReload
+    // configNoPlayerReload
 
-    // Keepsake.PlayerPickupableItemHandler
-    //Handle_AmmoInMagChanged(int ammoInMag)
     [HarmonyPatch(typeof(PlayerPickupableItemHandler), nameof(PlayerPickupableItemHandler.Handle_AmmoInMagChanged))]
     class ItemHandlerPatch1
     {
         static bool Prefix(ref PlayerPickupableItemHandler __instance, ref int ammoInMag)
         {
             if (!configNoPlayerReload.Value)
-            {
-                //Log.LogInfo("PLAYER RELOAD NORMAL");
                 return true;
-            }
             int maxAmmoInMag = __instance.ItemHeldPersistentPickupable.PickupableBlackboardData.m_MagSize.Value;
             int currentAmmoInMag = __instance.ItemHeldPersistentPickupable.PickupableBlackboardData.m_AmmoInMag.Value;
-            //Log.LogInfo("PLAYER NO RELOAD - Current bulletcount: " + __instance.CurrentAmmo + " max ammo " + __instance.MaxAmmo + " max ammo in mag: " + maxAmmoInMag  + " new ammo in mag: " + currentAmmoInMag);
             if (currentAmmoInMag < maxAmmoInMag)
                 __instance.ItemHeldPersistentPickupable.PickupableBlackboardData.m_AmmoInMag.Value = maxAmmoInMag;
-            //Log.LogInfo("PLAYER AMMO COST ZERO - Current bulletcount: " + __instance.m_CurrentBulletCount.ToString());
             return true;
         }
     }
@@ -368,81 +336,55 @@ public class Plugin : BasePlugin
         static void Postfix(ref PickupableItem_Railgun __instance)
         {
             if (!configNoPlayerReload.Value)
-            {
                 return;
-            }
-            //Log("RAILGUN NO RELOAD - Current resource: " + __instance.m_PickupableItemBlackboardData.m_ResourceAmount.Value);
-            //Log("RAILGUN NO RELOAD - Max resource to carry: " + __instance.m_ItemData.m_MaxResourceAmountToCarry);
             __instance.m_PickupableItemBlackboardData.m_ResourceAmount.Value = __instance.m_ItemData.m_MaxResourceAmountToCarry;
         }
     }
-    // Il2CppKeepsake.HyperSpace.GameplayFeatures.Pickupables.RocketLauncher.PickupableItem_RPG_FirstPerson
-
     [HarmonyPatch(typeof(PickupableItem_RPG_FirstPerson), nameof(PickupableItem_RPG_FirstPerson.FireReleased))]
     class RPGPatch1
     {
         static void Postfix(ref PickupableItem_RPG_FirstPerson __instance)
         {
             if (!configNoPlayerReload.Value)
-            {
                 return;
-            }
-            //Log("RPG NO RELOAD - Current resource: " + __instance.m_PickupableItemBlackboardData.m_ResourceAmount.Value);
-            //Log("RPG NO RELOAD - Max resource to carry: " + __instance.m_ItemData.m_MaxResourceAmountToCarry);
             __instance.m_PickupableItemBlackboardData.m_ResourceAmount.Value = __instance.m_ItemData.m_MaxResourceAmountToCarry;
         }
     }
 
-
     // configPlayerDamageMultiplier
 
-    // PickupableItemFirstPerson_Base !!
     [HarmonyPatch(typeof(PickupableItemFirstPerson_Base), nameof(PickupableItemFirstPerson_Base.ShootProjectile))]
     class FPGunPatch1
     {
         static bool Prefix(ref float interiorDamage, ref float shipDamage)
         {
             if (configPlayerDamageMultiplier.Value <= 1f)
-            {
-                //Log.LogInfo("PLAYER DAMAGE MULTIPLIER NORMAL");
                 return true;
-            }
             interiorDamage *= configPlayerDamageMultiplier.Value;
             shipDamage *= configPlayerDamageMultiplier.Value;
-            //Log.LogInfo("PLAYER DAMAGE MULTIPLIER " + configPlayerDamageMultiplier.Value.ToString() + " - Interior damage: " + interiorDamage + " - Ship damage: " + shipDamage);
             return true;
         }
     }
-
-    //PlayerMeleeHandler 
     [HarmonyPatch(typeof(PlayerMeleeHandler), nameof(PlayerMeleeHandler.GetDamage))]
     class MeleePatch1
     {
         static void Postfix(ref PlayerMeleeHandler __instance, ref float __result)
         {
             if (configPlayerDamageMultiplier.Value <= 1f)
-            {
-                //Log.LogInfo("PLAYER MELEE DAMAGE MULTIPLIER NORMAL - Multiplier value: " + __result.ToString());
                 return;
-            }
             __result *= configPlayerDamageMultiplier.Value;
-            //Log.LogInfo("PLAYER MELEE DAMAGE MULTIPLIER " + configPlayerDamageMultiplier.Value.ToString() + " - Multiplier value: " + __result.ToString());
         }
     }
 
     // configInstantBoost
 
-    // SpaceShip_EngineController
-    // BoostRechargeTime
     [HarmonyPatch(typeof(SpaceShip_EngineController), nameof(SpaceShip_EngineController.BoostRechargeTime), MethodType.Getter)]
     class Player_ShipBoostPatch1
     {
         static void Postfix(ref float __result)
         {
             if (!configInstantBoost.Value)
-            {
                 return;
-            }
             __result = 0;
 
         }
@@ -456,87 +398,70 @@ public class Plugin : BasePlugin
         static void Postfix(ref bool __result)
         {
             if (!configInfiniteJump.Value)
-            {
                 return;
-            }
             if (Keepsake.HyperSpace.NewInputSystem.InputManager.IsActionPressed(Keepsake.HyperSpace.NewInputSystem.InputManager.InputKeys.Jump))
-            {
                 __result = true;
-            }
-
         }
     }
 
     // configPlayerSpeedMultiplier
 
-    // Il2Cpp.Player_MovementHandler
-    [HarmonyPatch(typeof(Player_MovementHandler), nameof(Player_MovementHandler.AfterCharacterUpdate))]
+    private static float maxSlideSpeed = 16f;
+
+    [HarmonyPatch(typeof(Player_MovementHandler), nameof(Player_MovementHandler.Awake))]
     class LocalVelocityPatch1
     {
         static void Postfix(ref Player_MovementHandler __instance)
         {
-            if (configPlayerSpeedMultiplier.Value <= 1 || !__instance.IsOnGround)
-            {
-                return;
-            }
-            Vector3 vectorMult = new Vector3(configPlayerSpeedMultiplier.Value, configPlayerSpeedMultiplier.Value, configPlayerSpeedMultiplier.Value);
-            Vector3 localV = Vector3.Scale(__instance.LocalMovementVelocity, vectorMult);
-            float maxSpeed = __instance.MaxMovementSpeed * configPlayerSpeedMultiplier.Value;
-            localV = Vector3.ClampMagnitude(localV, maxSpeed);
-            //Log("MovementHandler sprint local velocity multiplied: " + localV.magnitude + " sprint time " + __instance.SprintElapsedTime);
-            __instance.LocalMovementVelocity = localV;
+            maxSlideSpeed = (float)__instance?.m_PlayerController?.SlideSettings?.m_MaxSlideSpeed;
         }
     }
 
-    //configBoostTimeMult
+    [HarmonyPatch(typeof(Player_MovementHandler), nameof(Player_MovementHandler.AfterCharacterUpdate))]
+    class LocalVelocityPatch2
+    {
+        static void Postfix(ref Player_MovementHandler __instance)
+        {
+            if (configPlayerSpeedMultiplier.Value <= 1f || !__instance.IsOnGround)
+                return;
+            Vector3 vectorMult = new Vector3(configPlayerSpeedMultiplier.Value, configPlayerSpeedMultiplier.Value, configPlayerSpeedMultiplier.Value);
+            Vector3 localV = Vector3.Scale(__instance.LocalMovementVelocity, vectorMult);
+            float maxSpeed = __instance.MaxMovementSpeed * configPlayerSpeedMultiplier.Value;
+            if (__instance.m_PlayerBlackboardData.m_IsSliding.Value)
+                __instance.m_PlayerController.SlideSettings.m_MaxSlideSpeed = maxSlideSpeed * configPlayerSpeedMultiplier.Value;
+            localV = Vector3.ClampMagnitude(localV, maxSpeed);
+            if (!__instance.m_PlayerBlackboardData.m_IsSliding.Value)
+                __instance.LocalMovementVelocity = localV;
+        }
+    }
 
-    // BoostRechargeTime
+    // configBoostTimeMult
+
     [HarmonyPatch(typeof(SpaceShip_EngineController), nameof(SpaceShip_EngineController.BoostTime), MethodType.Getter)]
     class Player_ShipBoostPatch2
     {
         static void Postfix(ref float __result)
         {
             if (configBoostTimeMult.Value <= 1)
-            {
                 return;
-            }
             __result *= configBoostTimeMult.Value;
-
         }
     }
 
-    // Keepsake.PersistentPickupable
+    // configIngotMultiplier
 
-    // Keepsake.PlayerPickupableItemHandler PersistentPickupable GetPersistentObjectFromItemData(PickupableItem_Data itemData)
-    /*[HarmonyPatch(typeof(PlayerPickupableItemHandler), nameof(PlayerPickupableItemHandler.GetPersistentObjectFromItemData))]
-    class IngotValuePatch1
-    {
-        static bool Prefix(ref PickupableItem_Data itemData, ref PlayerPickupableItemHandler __instance)
-        {
-            if (configIngotMultiplier.Value <= 1 || itemData == null)
-            {
-                return true;
-            }
-            itemData.m_Ingots *= configIngotMultiplier.Value;
-            Log.LogInfo("INGOT VALUE MULTIPLIER: " + configIngotMultiplier.Value.ToString() + " - Item value: " + itemData.m_Ingots.ToString());
-            return true;
-        }
-    }*/
-
-    // Keepsake.HyperSpace.GameplayFeatures.Campaigns.MissionData
     [HarmonyPatch(typeof(MissionData), nameof(MissionData.GetIngotsReward))]
     class IngotValuePatch2
     {
         static void Postfix(ref int __result)
         {
             if (configIngotMultiplier.Value <= 1)
-            {
                 return;
-            }
-            __result = (int)(__result * configIngotMultiplier.Value);
-            //Log.LogInfo("INGOT VALUE MULTIPLIER 2 (mission): " + configIngotMultiplier.Value.ToString() + " - Ingots: " + __result);
+            __result = Mathf.RoundToInt(__result * configIngotMultiplier.Value);
         }
     }
+
+    // configCreditsMultiplier
 
     [HarmonyPatch(typeof(MissionData), nameof(MissionData.GetCreditsReward))]
     class CreditsRewardPatch1
@@ -544,26 +469,21 @@ public class Plugin : BasePlugin
         static void Postfix(ref int __result)
         {
             if (configCreditsMultiplier.Value <= 1)
-            {
                 return;
-            }
-            __result = (int)(__result * configCreditsMultiplier.Value);
-            //Log.LogInfo("CREDITS VALUE MULTIPLIER 2 (mission): " + configCreditsMultiplier.Value.ToString() + " - Credits: " + __result);
+            __result = Mathf.RoundToInt(__result * configCreditsMultiplier.Value);
         }
     }
 
-    //configPlayerXPMultiplier
+    // configPlayerXPMultiplier
+
     [HarmonyPatch(typeof(MissionData), nameof(MissionData.GetExperienceReward))]
     class XPRewardPatch1
     {
         static void Postfix(ref int __result)
         {
             if (configPlayerXPMultiplier.Value <= 1)
-            {
                 return;
-            }
-            __result = (int)(__result * configPlayerXPMultiplier.Value);
-            //Log.LogInfo("XP VALUE MULTIPLIER 2 (mission): " + configPlayerXPMultiplier.Value.ToString() + " - Experience: " + __result);
+            __result = Mathf.RoundToInt(__result * configPlayerXPMultiplier.Value);
         }
     }
 
